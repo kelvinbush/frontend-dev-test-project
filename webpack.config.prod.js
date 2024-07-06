24e2127c07bc37950bfeb2dc3ad927a7ee7ad63d
Kelvin Wachiye <kelybush@gmail.com>
2024-07-06 18:54:14 +0300

    set up config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,6 @@ module.exports = merge(common, {
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'img', to: 'img' },
         { from: 'css', to: 'css' },
         { from: 'js/vendor', to: 'js/vendor' },
         { from: 'icon.svg', to: 'icon.svg' },
